2f782fd5f4ad8230bec6c7224cd99ec82c6dee43
Tati <tatka.bogat@gmail.com>
2025-06-26 00:31:54 +0200

    add homework Java 06.19

---
+++ src/main/java/prof/homeworks/_06_19_Lesson2/task3/Main.java
@@ -0,0 +1,18 @@
+package prof.homeworks._06_19_Lesson2.task3;
+/*
+Задача 3. Обработка команд от пользователя
+Описание:
+Создайте интерфейс Command с методом void execute().
+Реализуйте несколько классов-команд:
+PrintHello — выводит "Hello!"
+PrintDate — выводит текущую дату (можно просто System.out.println("Today is June 19") для простоты)
+PrintBye — выводит "Bye!"
+Требуется:
+Поместите команды в массив Command[].
+Создайте метод runAll(Command[] commands), который вызывает метод execute() у каждой команды.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

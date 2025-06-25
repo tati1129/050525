08b63871a6a85a832e92da18b54d9be986a7bb85
Tati <tatka.bogat@gmail.com>
2025-06-26 00:31:54 +0200

    add homework Java 06.19

---
+++ src/main/java/prof/homeworks/_06_19_Lesson2/task3/Command.java
@@ -0,0 +1,5 @@
+package prof.homeworks._06_19_Lesson2.task3;
+
+public interface Command {
+    public void execute();
+}
---
+++ src/main/java/prof/homeworks/_06_19_Lesson2/task3/Task.java
@@ -0,0 +1,45 @@
+package prof.homeworks._06_19_Lesson2.task3;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Task {
+
+    public static class PrintHello implements Command{
+
+        @Override
+        public void execute() {
+            System.out.println("Hello!");
+        }
+    }
+    public static class PrintDate implements Command{
+
+        @Override
+        public void execute() {
+            System.out.println("Today's date: " + LocalDate.now());
+            System.out.println("Today's date: " + LocalTime.now());
+            //System.out.println(LocalDateTime.now());
+        }
+    }
+    public static class PrintBye implements Command{
+
+        @Override
+        public void execute() {
+            System.out.println("Buy!");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();
+        PrintDate printDate = new PrintDate();
+        PrintBye printBye = new PrintBye();
+
+        Command[] commands = {printHello, printDate,printBye};
+
+        for (Command command : commands){
+            command.execute();
+        }
+
+    }
+}
